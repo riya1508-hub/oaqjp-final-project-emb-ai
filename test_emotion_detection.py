072be1d5bd441499a5eb6a49decc2af7f80055ad
riya1508-hub <riya.1044742@gmail.com>
2026-09-04 11:45:18 +0200

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,14 +1,43 @@
+import unittest
 from EmotionDetection import emotion_detector
 
 
-def test_emotion_detector():
-    assert emotion_detector("I am glad this happened")["dominant_emotion"] == "joy"
-    assert emotion_detector("I am really mad about this")["dominant_emotion"] == "anger"
-    assert emotion_detector("I feel disgusted just hearing about this")["dominant_emotion"] == "disgust"
-    assert emotion_detector("I am so sad about this")["dominant_emotion"] == "sadness"
-    assert emotion_detector("I am really afraid that this will happen")["dominant_emotion"] == "fear"
+class TestEmotionDetector(unittest.TestCase):
+
+    def test_joy(self):
+        self.assertEqual(
+            emotion_detector("I am glad this happened")["dominant_emotion"],
+            "joy"
+        )
+
+    def test_anger(self):
+        self.assertEqual(
+            emotion_detector("I am really mad about this")["dominant_emotion"],
+            "anger"
+        )
+
+    def test_disgust(self):
+        self.assertEqual(
+            emotion_detector(
+                "I feel disgusted just hearing about this"
+            )["dominant_emotion"],
+            "disgust"
+        )
+
+    def test_sadness(self):
+        self.assertEqual(
+            emotion_detector("I am so sad about this")["dominant_emotion"],
+            "sadness"
+        )
+
+    def test_fear(self):
+        self.assertEqual(
+            emotion_detector(
+                "I am really afraid that this will happen"
+            )["dominant_emotion"],
+            "fear"
+        )
 
 
 if __name__ == "__main__":
-    test_emotion_detector()
-    print("All unit tests passed!")
+    unittest.main()
